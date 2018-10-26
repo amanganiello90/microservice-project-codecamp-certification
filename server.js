42f7a0b7532e938619b5dbf8a934f3f21774c68c
Glitch (microservice-project-codecamp-certification) <none>
2018-10-26 13:46:30 +0000

    🎩🎨 Updated with Glitch boilerplate

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 // server.js
 // where your node app starts
-
+var myApp = require('./myApp');
 // init project
 const express = require('express');
 const app = express();
 
+if (!process.env.DISABLE_XORIGIN) {
+  app.use(function(req, res, next) {
+    var allowedOrigins = ['https://narrow-plane.gomix.me', 'https://www.freecodecamp.com'];
+    var origin = req.headers.origin || '*';
+    if(!process.env.XORIG_RESTRICT || allowedOrigins.indexOf(origin) > -1){
+         console.log(origin);
+         res.setHeader('Access-Control-Allow-Origin', origin);
+         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    }
+    next();
+  });
+}
+var port = process.env.PORT || 3000;
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
-console.log('Hello World');
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -18,6 +30,7 @@ app.get('/', function(request, response) {
 });
 
 // listen for requests :)
-const listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
+// listen for requests :)
+const listener = app.listen(port, function() {
+  console.log('Your app is listening on port ' + port);
 });
